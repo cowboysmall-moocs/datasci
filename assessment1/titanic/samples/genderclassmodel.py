987668456ee06f9ee9a5eeac8d0bc4178a730e57
Jerry Kiely <jerry@cowboysmall.com>
2014-08-13 20:42:04 -0400

    re-arranged data and sample code in titanic project

--- assessment1/titanic/samples/genderclassmodel.py
+++ assessment1/titanic/samples/genderclassmodel.py
@@ -17,6 +17,11 @@ for row in csv_file_object:                 # Skip through each row in the csv f
     data.append(row)                        # adding each row to the data variable
 data = np.array(data)                       # Then convert from a list to an array
 
+
+
+
+
+
 # In order to analyse the price column I need to bin up that data
 # here are my binning parameters, the problem we face is some of the fares are very large
 # So we can either have a lot of bins with nothing in them or we can just lose some
@@ -31,6 +36,9 @@ number_of_price_brackets = fare_ceiling / fare_bracket_size
 number_of_classes = 3                             # I know there were 1st, 2nd and 3rd classes on board.
 number_of_classes = len(np.unique(data[0::,2]))   # But it's better practice to calculate this from the Pclass directly:
                                                   # just take the length of an array of UNIQUE values in column index 2
+
+
+
 
 
 # This reference matrix will show the proportion of survivors as a sorted table of
@@ -57,15 +65,29 @@ for i in xrange(number_of_classes):
         survival_table[0,i,j] = np.mean(women_only_stats.astype(np.float))  # Female stats
         survival_table[1,i,j] = np.mean(men_only_stats.astype(np.float))    # Male stats
 
+
+
+
+
 # Since in python if it tries to find the mean of an array with nothing in it
 # (such that the denominator is 0), then it returns nan, we can convert these to 0
 # by just saying where does the array not equal the array, and set these to 0.
 survival_table[ survival_table != survival_table ] = 0.
 
+
+
+
+
+
 # Now I have my proportion of survivors, simply round them such that if <0.5
 # I predict they dont surivive, and if >= 0.5 they do
 survival_table[ survival_table < 0.5 ] = 0
 survival_table[ survival_table >= 0.5 ] = 1
+
+
+
+
+
 
 # Now I have my indicator I can read in the test file and write out
 # if a women then survived(1) if a man then did not survived (0)
@@ -73,6 +95,11 @@ survival_table[ survival_table >= 0.5 ] = 1
 test_file = open('test.csv', 'rb')
 test_file_object = csv.reader(test_file)
 header = test_file_object.next()
+
+
+
+
+
 
 # Also open the a new file so I can write to it. 
 predictions_file = open("genderclassmodel.csv", "wb")
@@ -107,6 +134,10 @@ for row in test_file_object:
         predictions_file_object.writerow([row[0], "%d" % int(survival_table[ 0, float(row[1]) - 1, bin_fare ])])
     else:
         predictions_file_object.writerow([row[0], "%d" % int(survival_table[ 1, float(row[1]) - 1, bin_fare])])
+
+
+
+
 
 # Close out the files
 test_file.close()
