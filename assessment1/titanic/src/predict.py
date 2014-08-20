69fc1983e2e7506b2f082c8ddb9b274d0e35c969
Jerry Kiely <jerry@cowboysmall.com>
2014-08-20 08:37:38 -0400

    cleaned up predict

--- assessment1/titanic/src/predict.py
+++ assessment1/titanic/src/predict.py
@@ -5,7 +5,6 @@ from sklearn import preprocessing, cross_validation, metrics, dummy
 from clean import clean_data
 
 
-
 def fit_dummy(X, y):
     clf = dummy.DummyClassifier()
     clf.fit(X, y)
@@ -13,49 +12,32 @@ def fit_dummy(X, y):
     return clf
 
 
-
-def make_prediction(df_train, df_test, fit_model, output_file):
-    X         = df_train.drop(['PassengerId', 'Survived'], 1)
-    y         = df_train['Survived']
-    X         = preprocessing.scale(X)
-
-
-    data_train, data_test, target_train, target_test = cross_validation.train_test_split(X, y, train_size = 0.8)
-
-
-    clf     = fit_model(data_train, target_train)
-    results = clf.predict(data_test)
-
-
+def cross_validation_score(clf, X, y, results, name):
     print
-    print 'Cross Validation: Test Data'
+    print 'Cross Validation: ', name
     print
-    print metrics.classification_report(target_test, results)
-    print
-    print 'Cross Validation: Score = ', cross_validation.cross_val_score(clf, data_test, target_test).mean()
-    print
-
-
-    clf     = fit_model(X, y)
-    results = clf.predict(X)
-
-
-    print
-    print 'Cross Validation: Training Data'
-    print
-    print metrics.classification_report(y.values, results)
+    print metrics.classification_report(y, results)
     print
     print 'Cross Validation: Score = ', cross_validation.cross_val_score(clf, X, y).mean()
     print
 
 
-    X   = df_test.drop(['PassengerId'], 1)
-    X   = preprocessing.scale(X)
-    ids = df_test['PassengerId'].values
+def make_prediction(df_train, df_test, fit_model = fit_dummy, output_file = './output/prediction_dummy.csv'):
+    X = preprocessing.scale(df_train.drop(['PassengerId', 'Survived'], 1))
+    y = df_train['Survived']
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, train_size = 0.5)
+
+    clf     = fit_model(X_train, y_train)
+    results = clf.predict(X_test)
+    cross_validation_score(clf, X_test, y_test, results, 'Test Data')
 
 
-    results = clf.predict(X)
+    X_predict = preprocessing.scale(df_test.drop(['PassengerId'], 1))
+    ids       = df_test['PassengerId']
 
+    clf     = fit_model(X, y)
+    results = clf.predict(X_predict)
 
     with open(output_file, 'wb') as csvfile:
         writer = csv.writer(csvfile)
@@ -64,13 +46,11 @@ def make_prediction(df_train, df_test, fit_model, output_file):
             writer.writerow([ids[i], results[i]])
 
 
-
 def main(argv):
     df_train = clean_data(argv[0])
     df_test  = clean_data(argv[1])
 
-    make_prediction(df_train, df_test, fit_dummy, './output/prediction_dummy.csv')
-
+    make_prediction(df_train, df_test)
 
 
 if __name__ == '__main__':
