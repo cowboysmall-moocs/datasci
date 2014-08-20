4cbef581dde201f1a36e073c03d859fe01d1177a
Jerry Kiely <jerry@cowboysmall.com>
2014-08-20 10:02:13 -0400

    enhancements to parameters selecting

--- assessment1/titanic/src/parameters_select.py
+++ assessment1/titanic/src/parameters_select.py
@@ -6,21 +6,28 @@ from clean import clean_data
 
 def select_parameters(X, y):
     parameters_grid = [
-        {'C': [1, 5, 10, 50, 100, 1000], 'kernel': ['linear']},
-        {'C': [1, 5, 10, 50, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.5, 0.25, 0.1, 0.01, 0.001, 0.0001]},
-        {'C': [1, 5, 10, 50, 100, 1000], 'kernel': ['poly'], 'gamma': [0.5, 0.25, 0.1, 0.01, 0.001, 0.0001], 'degree': [2, 3]},
-        {'C': [1, 5, 10, 50, 100, 1000], 'kernel': ['sigmoid'], 'gamma': [0.5, 0.25, 0.1, 0.01, 0.001, 0.0001]},
+        {'C': [1, 3, 5, 10, 25, 50, 100, 250, 500], 'kernel': ['rbf'], 'gamma': [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001, 0.0001]},
+        {'C': [1, 3, 5, 10, 25, 50, 100, 250, 500], 'kernel': ['sigmoid'], 'gamma': [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001, 0.0001]},
     ]
     clf = svm.SVC()
-    gs  = grid_search.GridSearchCV(svm.SVC(), parameters_grid)
+    gs  = grid_search.GridSearchCV(clf, parameters_grid)
     gs.fit(X, y)
 
-    print
-    print clf.get_params()
-    print
+    best  = 0.0
+    score = None
+    for grid_score in gs.grid_scores_:
+        if best < grid_score[1]:
+            best  = grid_score[1]
+            score = grid_score[0]
 
     print
-    print gs.get_params()
+    print 'Best Grid Score:'
+    print
+    print '%s: C = %5s, gamma = %2.4f [mean = %2.6f]' % (score['kernel'], score['C'], score['gamma'], best)
+    print
+    print 'Best Estimator:'
+    print
+    print gs.best_estimator_
     print
 
 
