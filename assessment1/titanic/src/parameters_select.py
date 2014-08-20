67969d2e605fe81fa3fd3e92409617f1f0073cc4
Jerry Kiely <jerry@cowboysmall.com>
2014-08-20 10:39:02 -0400

    enhanced parameter selection + updated svm based on the results

--- assessment1/titanic/src/parameters_select.py
+++ assessment1/titanic/src/parameters_select.py
@@ -1,17 +1,24 @@
 import sys
 
-from sklearn import grid_search, svm
+from sklearn import preprocessing, grid_search, svm
 from clean import clean_data
 
 
-def select_parameters(X, y):
+def select_parameters(df_train, df_test):
+    X = preprocessing.scale(df_train.drop(['PassengerId', 'Survived'], 1))
+    y = df_train['Survived']
+
     parameters_grid = [
         {'C': [1, 3, 5, 10, 25, 50, 100, 250, 500], 'kernel': ['rbf'], 'gamma': [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001, 0.0001]},
         {'C': [1, 3, 5, 10, 25, 50, 100, 250, 500], 'kernel': ['sigmoid'], 'gamma': [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001, 0.0001]},
     ]
+
     clf = svm.SVC()
     gs  = grid_search.GridSearchCV(clf, parameters_grid)
     gs.fit(X, y)
+
+    X = preprocessing.scale(df_test.drop(['PassengerId'], 1))
+    gs.predict(X)
 
     best  = 0.0
     score = None
@@ -23,7 +30,7 @@ def select_parameters(X, y):
     print
     print 'Best Grid Score:'
     print
-    print '%s: C = %5s, gamma = %2.4f [mean = %2.6f]' % (score['kernel'], score['C'], score['gamma'], best)
+    print '%s: C = %3s, gamma = %0.4f [mean = %0.8f]' % (score['kernel'], score['C'], score['gamma'], best)
     print
     print 'Best Estimator:'
     print
@@ -33,12 +40,10 @@ def select_parameters(X, y):
 
 
 def main(argv):
-    df = clean_data(argv[0])
+    df_train = clean_data(argv[0])
+    df_test  = clean_data(argv[1])
 
-    X  = df.drop(['PassengerId', 'Survived'], 1)
-    y  = df['Survived']
-
-    select_parameters(X, y)
+    select_parameters(df_train, df_test)
 
 
 
